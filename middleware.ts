1b825698e3fe36ec122ca32353b1cc9fec9017f6
Mbappe <tymeless@Host-001.lan>
2026-05-29 15:18:42 +0200

    fix: middleware check supabase cookie

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
-import { createClient } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   const token = req.cookies.get('sb-access-token')?.value
-  
+    || req.cookies.get('supabase-auth-token')?.value
+    || req.cookies.get('sb-jigwcrybfhvlmzoclbih-auth-token')?.value
+
   if (!token && req.nextUrl.pathname.startsWith('/dashboard')) {
     return NextResponse.redirect(new URL('/login', req.url))
   }
